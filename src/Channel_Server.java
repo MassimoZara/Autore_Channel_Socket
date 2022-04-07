0ab1451a81fcaa42c1744b26ec2e2671221119b3
Massimo Zarantonello <massimo.zaranto@gmail.com>
2022-04-07 16:59:16 +0200

    Creato workspace con file client e server

---
+++ src/Channel_Server.java
@@ -0,0 +1,13 @@
+
+public class Channel_Server {
+
+	public Channel_Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Channel_Socket.java
@@ -0,0 +1,13 @@
+
+public class Channel_Socket {
+
+	public Channel_Socket() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
